38ed6d228165d0b8b3e15f1616e2bb5b2c1b6970
José Alfredo Arroyo Gómez <arroyo.alfredo.1pm@gmail.com>
2024-06-05 23:58:44 -0600

    91. Mostrando los Pacientes Agregados

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1 class="font-black text-5xl text-center">Seguimiento Pacientes <span class="text-indigo-600">Veterianrias</span>
+    </h1>
+</template>
